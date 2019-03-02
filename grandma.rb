4866cb09c1ab51c12864ae7cc5a383c1f49196e8
Christopher Luna <chris@illusionlayervfx.com>
2019-03-02 17:47:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase = "Hey Ma")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.uppercase ==
     return "NO, NOT SINCE 1938!"
-    
+  else
+    return "HUH?! SPEAK UP, SONNY!"  
   end
 end
 # HUH?! SPEAK UP, SONNY!
